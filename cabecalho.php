b9a41a387123600d9059afd769ac77af09737bba
Mikael Fontoura <mikaelfontoura29@gmail.com>
2023-11-28 22:30:54 -0300

    Arrumando cabeçalho

--- cabecalho.php
+++ cabecalho.php
@@ -1,10 +1,28 @@
 <?php if (isset($_SESSION)) { ?>
-  <nav class="navbar navbar-lingt" style="background-color:gold">
-    <div class="container-fluid" style="margin-left: 30px">
-      <a class="navbar-brand" href="index.php">
-        <img src="" alt="logo" width="30" height="24" class="d-inline-block align-text-top">
-        <font style="vertical-align: inherit;"> Biblioteca Jurídica </font>
-      </a>
+  <div class="row">
+    <header style="background-color:gold">
+      <nav class="navbar navbar-expand-sm">
+        <div class="container p-1 mb-2">
+          <a class="text-dark link-body-emphasis text-decoration-none" href="index.php">
+            <img src="" alt="Logo" width="auto" height="50px" class="navbar-brand me-3">
+            Biblioteca Jurídica</a>
+        </div>
+        <ul class="navbar-nav">
+          <li class="nav-link"><a class="nav-link link-body-emphasis text-light" href="#">Conteúdo</a></li>
+        </ul>
+        <div class="dropdown">
+          <a class="d-block link-body-emphasis text-decoration-none dropdown-toggle text-light" data-bs-toggle="dropdown" aria-expanded="false">Conta</a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="logout.php">Sair</a></li>
+            <li><a class="dropdown-item" href="edit_usuario.php?id_usuario=<?php echo $_SESSION['id_usuario']; ?>">Editar Conta</a></li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  </div>
+  <nav class="navbar navbar-lingt">
+    <div class="container-fluid">
+
       <nav class="navbar navbar-expand-lg" style="background-color: gold">
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-3 mb-lg-8">
