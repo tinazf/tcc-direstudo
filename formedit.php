920d20c99f6f6f242e96113d5088ec641e2e7d9b
Mikael Fontoura <mikaelfontoura29@gmail.com>
2023-12-03 18:44:59 -0300

    Correção

--- formedit.php
+++ formedit.php
@@ -1,21 +1,15 @@
 <?php
 require_once "conexao.php";
 $conecta = conectar();
-$id = $_POST['id_usuario'];
-
-if (isset($_POST['editar'])) {
-    $sql = "SELECT * FROM usuario WHERE id_usuario=" . $id;
-    $stmt = mysqli_prepare($conecta, $sql);
-    mysqli_stmt_bind_param($stmt, "i", $id);
-    mysqli_stmt_execute($stmt);
-    $resultado = mysqli_stmt_get_result($stmt);
-
-    if (!$resultado) {
-        die('Erro na consulta: ' . mysqli_error($conecta));
-    }
-
+$id = $_GET['id_usuario'];
+$sql = "SELECT * FROM usuario WHERE id_usuario=" . $id;
+$resultado = mysqli_query($conecta, $sql);
+if (!$resultado) {
+    die('Erro na consulta: ' . mysqli_error($conecta));
+} else {
     $dados = mysqli_fetch_assoc($resultado);
-
+}
+if (isset($_POST['editar'])) {
     if (isset($_POST['id_usuario'], $_POST['nome'], $_POST['cpf'], $_POST['email'], $_POST['senha'])) {
         $id = $_POST['id_usuario'];
         $nome = $_POST['nome'];
@@ -24,16 +18,16 @@ if (isset($_POST['editar'])) {
         $senha = $_POST['senha'];
 
         $sql = "UPDATE usuario SET nome='$nome', cpf='$cpf', email='$email', senha='$senha' WHERE id_usuario=" . $id;
-        $stmt = mysqli_prepare($conecta, $sql);
-        mysqli_stmt_bind_param($stmt, "ssssi", $nome, $cpf, $email, $senha, $id);
-        mysqli_stmt_execute($stmt);
+        $resultado = mysqli_query($conecta, $sql);
     }
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 </head>
+
 <body>
     <hr>
     <div class="container">
@@ -59,7 +53,7 @@ if (isset($_POST['editar'])) {
 
                 <div class="form-group">
                     <label>Senha: </label>
-                    <input type="password" name="senha" value="<?php echo $dados['senha']; ?>"><br><br>
+                    <input type="password" name="senha"><br><br>
                 </div>
 
                 <div class="form-group">
@@ -69,8 +63,9 @@ if (isset($_POST['editar'])) {
         </div>
     </div>
     <?php include_once('rodape.php'); ?>
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
+
 </html>
