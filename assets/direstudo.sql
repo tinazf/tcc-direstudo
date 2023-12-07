a5ab2b66da1ee189f93dcb083497764b87d2a8fd
Mikael Fontoura <mikaelfontoura29@gmail.com>
2023-12-07 00:06:40 -0300

    Novo banco

--- assets/direstudo.sql
+++ assets/direstudo.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Tempo de geração: 29-Nov-2023 às 01:40
--- Versão do servidor: 5.7.36
--- versão do PHP: 7.4.26
+-- Host: 127.0.0.1
+-- Tempo de geração: 07-Dez-2023 às 04:06
+-- Versão do servidor: 10.4.25-MariaDB
+-- versão do PHP: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,20 +24,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `id_categoria` int(11) NOT NULL,
+  `categoria` varchar(255) NOT NULL,
+  `descricao` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `documento`
 --
 
-DROP TABLE IF EXISTS `documento`;
-CREATE TABLE IF NOT EXISTS `documento` (
-  `id_documento` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `documento` (
+  `id_documento` int(11) NOT NULL,
   `id_usuario` int(11) NOT NULL,
   `titulo` varchar(255) NOT NULL,
-  `categoria` varchar(255) NOT NULL,
+  `categoria` int(11) NOT NULL,
   `autor` varchar(255) NOT NULL,
   `descricao` varchar(255) NOT NULL,
-  `arquivo` varchar(255) NOT NULL,
-  PRIMARY KEY (`id_documento`),
-  KEY `id_usuario` (`id_usuario`)
+  `arquivo` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -46,15 +55,13 @@ CREATE TABLE IF NOT EXISTS `documento` (
 -- Estrutura da tabela `usuario`
 --
 
-DROP TABLE IF EXISTS `usuario`;
-CREATE TABLE IF NOT EXISTS `usuario` (
-  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
   `nome` varchar(255) NOT NULL,
   `cpf` varchar(20) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `senha` varchar(255) NOT NULL,
-  PRIMARY KEY (`id_usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+  `senha` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Extraindo dados da tabela `usuario`
@@ -64,6 +71,52 @@ INSERT INTO `usuario` (`id_usuario`, `nome`, `cpf`, `email`, `senha`) VALUES
 (1, 'Tina', '07789012053', 'tina716@gmail.com', '$2y$10$Y/ztF5t73XdWYsSYSqZ4O.03GDqq2lEUAoVykKbixro6EUnPDN142');
 
 --
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id_categoria`);
+
+--
+-- Índices para tabela `documento`
+--
+ALTER TABLE `documento`
+  ADD PRIMARY KEY (`id_documento`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `categoria` (`categoria`);
+
+--
+-- Índices para tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `documento`
+--
+ALTER TABLE `documento`
+  MODIFY `id_documento` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- Restrições para despejos de tabelas
 --
 
@@ -71,7 +124,8 @@ INSERT INTO `usuario` (`id_usuario`, `nome`, `cpf`, `email`, `senha`) VALUES
 -- Limitadores para a tabela `documento`
 --
 ALTER TABLE `documento`
-  ADD CONSTRAINT `documento_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
+  ADD CONSTRAINT `documento_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`),
+  ADD CONSTRAINT `documento_ibfk_2` FOREIGN KEY (`categoria`) REFERENCES `categoria` (`id_categoria`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
